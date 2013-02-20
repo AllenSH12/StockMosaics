763cf41e96a05281032a9d5685b001b02f539c19
Allen <sogishernandez.allen@gmail.com>
2013-02-20 11:53:12 -0600

    adjustments

--- product/fin-functions.js
+++ product/fin-functions.js
@@ -1,9 +1,14 @@
-	function findPriceTarget(valueOne, valueTwo) {
-		var priceTarget = valueOne / valueTwo;
-		return priceTarget;
-	}
+function sumVariables(a,b,c,d) {
+	var total = a + b + c + d;
+	return total;
+}
 
-	function findEV(TotSumDiscountedCashFlows,TermVal,CashEq,TotDebt) {
-		var EV = TotSumDiscountedCashFlows + TermVal - CashEq + TotDebt;
-		return EV;
-	}
+function findPriceTarget(valueOne, valueTwo) {
+	var priceTarget = valueOne / valueTwo;
+	return priceTarget;
+}
+
+function findEV(TotSumDiscountedCashFlows,TermVal,CashEq,TotDebt) {
+	var EV = TotSumDiscountedCashFlows + TermVal - CashEq + TotDebt;
+	return EV;
+}
